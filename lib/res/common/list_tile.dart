56da8d28d6333d512189cd9d7a743c364028e140
Ap00rvx <apurvabraj419@gmail.com>
2024-04-26 20:11:47 +0530

    Used Expandable tiles

--- lib/res/common/list_tile.dart
+++ lib/res/common/list_tile.dart
@@ -1,50 +1,97 @@
 import 'package:easy_lib/res/common/app_colors.dart';
 import 'package:easy_lib/src/model/book_model.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
-final makeListTile = (Book book) =>  ListTile(
-  enabled: book.available,
+final makeListTile = (Book book) =>  ExpansionTile(
     shape: RoundedRectangleBorder(
       borderRadius: BorderRadius.circular(20)
     ),
-    // tileColor: Colors.r,
+  children: [
+    Row(
+      children: [
+        SizedBox(
+          width: 10,
+        ),
 
-    contentPadding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+        Container(
+            padding: const EdgeInsets.only(right: 12.0),
+            child:   Padding(
+padding: const EdgeInsets.all(10.0),
+child: RotatedBox(quarterTurns: 3,child: Column(
+children: [
+Text("Price",style: TextStyle(color: AppColors.neutral),),
+Text(book.price.toString(),style: TextStyle(
+  color: book.available ? AppColors.black:Colors.grey.withOpacity(0.4)
+),),
+],
+),),
+),
+            decoration: const BoxDecoration(
+                border: Border(
+                    right: BorderSide(width: 1.0, color: Colors.white24)))),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text(book.description,style: TextStyle(
+            color: book.available ? AppColors.black:Colors.grey.withOpacity(0.4)
+          ),),
+        ),
+
+      ],
+    ),
+    const SizedBox(
+      height: 30,
+    ),
+    Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text("Published on -"+DateFormat("dd/MM/yyyy").format(book.publishedDate).toString(),style: TextStyle(
+            color:book.available ? AppColors.black:Colors.grey.withOpacity(0.4), fontWeight: FontWeight.w200,fontSize: 10)
+          ),
+          Text(book.available?"Available for issue" :"Issued to uid -"+ book.issuedTo.toString(),style: TextStyle(
+            color:book.available ? AppColors.positive :AppColors.red.withOpacity(0.2), fontWeight: FontWeight.w200)
+          ),
+        ],
+      ),
+    ),
+
+  ],
     leading: Container(
       padding: const EdgeInsets.only(right: 12.0),
       decoration: const BoxDecoration(
           border: Border(
               right: BorderSide(width: 1.0, color: Colors.white24))),
       child:  Text(book.bookId,textAlign: TextAlign.center,style: TextStyle(
-        fontWeight: book.available ? FontWeight.bold : FontWeight.normal
+        fontWeight: book.available ? FontWeight.bold : FontWeight.normal,color: book.available ? AppColors.black:Colors.grey.withOpacity(0.4)
       ),),
     ),
     title:  Text(
       book.name,
-      style: const TextStyle(color: AppColors.black, fontWeight: FontWeight.bold),
+      style:  TextStyle(color: book.available ? AppColors.black:Colors.grey.withOpacity(0.4), fontWeight: FontWeight.bold),
     ),
-
     subtitle:  Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: <Widget>[
         SizedBox(
           child: Row(
             children: [
-              Icon(Icons.linear_scale, color: book.available ? AppColors.positive:AppColors.red),
-              Text(book.authorName, style: const TextStyle(color: AppColors.black)),
+              Icon(Icons.linear_scale, color: book.available ? AppColors.positive:AppColors.red.withOpacity(0.2)),
+              Text(book.authorName, style:  TextStyle(color:book.available ? AppColors.black:Colors.grey.withOpacity(0.4))),
             ],
           ),
         ),
-        Text(book.genre, style:  TextStyle(color: Colors.grey.shade500)),
+        Text(book.available ? book.genre:"Not Available",  style:  TextStyle(color: Colors.grey.shade500)),
+
       ],
-    ),
-    trailing:
-    const Icon(Icons.keyboard_arrow_down, color:  AppColors.black, size: 30.0));
+    ));
+
 final customListTile =(Book book) => Card(
   elevation: 2,
   margin: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
   child: Container(
-    decoration: BoxDecoration(borderRadius: BorderRadius.circular(10),color: AppColors.black.withOpacity(0.2)),
+    decoration: BoxDecoration(borderRadius: BorderRadius.circular(10),color: book.available ? AppColors.black.withOpacity(0.2):Colors.grey.withOpacity(0.3)),
     child: makeListTile(book),
   ),
 );
